7e7863e990f2ac0b7b64524dfa265b34b04fa079
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 00:15:06 -0200

    - Testing FB.ui

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -4,5 +4,23 @@ function jmasq_main()
 	cipher_text = b.encrypt('plain text');
 	cipher_plain = b.decrypt(cipher_text);
 	alert(cipher_text + " / " + cipher_plain);
+
+	FB.ui(
+	  {
+		method: 'feed',
+		name: 'Facebook Dialogs',
+		link: 'https://developers.facebook.com/docs/reference/dialogs/',
+		picture: 'http://fbrell.com/f8.jpg',
+		caption: 'Reference Documentation',
+		description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
+	  },
+	  function(response) {
+		if (response && response.post_id) {
+		  alert('Post was published.');
+		} else {
+		  alert('Post was not published.');
+		}
+	  }
+	);
 }
 
