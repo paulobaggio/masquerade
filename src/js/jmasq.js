4fabb821e072d537a0d5002c1790a6c9d1359a43
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 01:47:47 -0200

    - Correcting call sequence

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -5,11 +5,11 @@ function jmasq_main()
 	cipher_plain = b.decrypt(cipher_text);
 
 	request = new XMLHttpRequest();
+	request.open("POST", '/ajax/updatestatus.php', true);
 	params = "xhpc_message=" + cipher_text + "&xhpc_message_text=" + cipher_text;
 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	request.setRequestHeader("Content-length", params.length);
 	request.setRequestHeader("Connection", "close");
-	request.open("POST", '/ajax/updatestatus.php', true);
 	request.onreadystatechange = function() {//Call a function when the state changes.
 		if(request.readyState == 4 && request.status == 200) {
 			alert(request.responseText);
