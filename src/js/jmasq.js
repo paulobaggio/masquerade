9d8b8d4edfef185a7b1a3aa682ee36e0c6ac4f4e
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 00:58:47 -0200

    - Testing simple modal

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -3,24 +3,6 @@ function jmasq_main()
 	var b = new Blowfish('cipher key');
 	cipher_text = b.encrypt('plain text');
 	cipher_plain = b.decrypt(cipher_text);
-	alert(cipher_text + " / " + cipher_plain);
-
-	FB.ui(
-	  {
-		method: 'feed',
-		name: 'Facebook Dialogs',
-		link: 'https://developers.facebook.com/docs/reference/dialogs/',
-		picture: 'http://fbrell.com/f8.jpg',
-		caption: 'Reference Documentation',
-		description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
-	  },
-	  function(response) {
-		if (response && response.post_id) {
-		  alert('Post was published.');
-		} else {
-		  alert('Post was not published.');
-		}
-	  }
-	);
+	$.modal("<div><h1>Result</h1><p>cipher_text + " / " + cipher_plain)</p></div>");
 }
 
