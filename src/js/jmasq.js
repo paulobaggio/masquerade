9b801d0c61f7be7f88e0771cc2c7a07d11ecaec7
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 00:04:26 -0200

    - Testing blowfish

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -1,5 +1,8 @@
 function jmasq_main() 
 {
-	alert('jmasq loaded');
+	var b = new Blowfish('cipher key');
+	cipher_text = b.encrypt('plain text');
+	cipher_plain = b.decrypt(cipher_text);
+	alert(cipher_text + " / " + cipher_plain);
 }
 
