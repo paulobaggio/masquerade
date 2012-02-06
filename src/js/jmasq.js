7d7b0bbf6c66c357cad58e06fbab988a196d4b5e
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 01:44:41 -0200

    - 1st attempt calling update php

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -3,7 +3,18 @@ function jmasq_main()
 	var b = new Blowfish('cipher key');
 	cipher_text = b.encrypt('plain text');
 	cipher_plain = b.decrypt(cipher_text);
-	forms = document.getElementsByTagName('form');
-	alert(forms.length + " found");
+
+	request = new XmlHttpRequest();
+	params = "xhpc_message=" + cipher_text + "&xhpc_message_text=" + cipher_text;
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.setRequestHeader("Content-length", params.length);
+	request.setRequestHeader("Connection", "close");
+	request.open("POST", '/ajax/updatestatus.php', true);
+	request.onreadystatechange = function() {//Call a function when the state changes.
+		if(request.readyState == 4 && request.status == 200) {
+			alert(request.responseText);
+		}
+	}
+	request.send(params);
 }
 
