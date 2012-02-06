ecec82058a9f35f45e5670638490b74a3f8398ad
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 00:26:14 -0200

    - Carregando api do facebook para UI

---
+++ src/js/load_fbui.js
@@ -0,0 +1,19 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : "Y2429789648", // App ID
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional initialization code here
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     d.getElementsByTagName('head')[0].appendChild(js);
+   }(document));
+
