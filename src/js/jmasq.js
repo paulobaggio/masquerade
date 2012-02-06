c6cc9b1b39a1853f75f8b0665cd489e7551dfe25
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 01:29:57 -0200

    - Testing for status update

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -3,6 +3,7 @@ function jmasq_main()
 	var b = new Blowfish('cipher key');
 	cipher_text = b.encrypt('plain text');
 	cipher_plain = b.decrypt(cipher_text);
-	$.modal("<div><h1>Result</h1><p>" + cipher_text + ' / ' + cipher_plain + "</p></div>");
+	forms = document.getElementsByTagName('form');
+	alert(forms.length + " found");
 }
 
