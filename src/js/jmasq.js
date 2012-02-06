f9be4bd0b9973d69ef25c574161c89ca7517b6a1
Diego Pereira <diego.pereira81@terra.com.br>
2012-02-06 01:05:47 -0200

    - Correcting text

--- src/js/jmasq.js
+++ src/js/jmasq.js
@@ -3,6 +3,6 @@ function jmasq_main()
 	var b = new Blowfish('cipher key');
 	cipher_text = b.encrypt('plain text');
 	cipher_plain = b.decrypt(cipher_text);
-	$.modal("<div><h1>Result</h1><p>cipher_text + ' / ' + cipher_plain)</p></div>");
+	$.modal("<div><h1>Result</h1><p>" + cipher_text + ' / ' + cipher_plain + "</p></div>");
 }
 
